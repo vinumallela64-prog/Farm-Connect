86be26738ff4c0e23d2d88022661a8a8196e5510
vinumallela64-prog <vinumallela64@gmail.com>
2026-03-25 01:04:42 +0530

    Update db.js

--- farmconnect/server/config/db.js
+++ farmconnect/server/config/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
+    const conn = await mongoose.connect(
+      "mongodb+srv://vinumallela64_db_user:RJxw7xN2lRremHgq@cluster0.a3bu1i6.mongodb.net/?appName=Cluster0"
+    );
+
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
